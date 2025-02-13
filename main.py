d3a44a458f00fc29d8216914feda97de0e5f6b5c
noliukov <noliukov@payler.com>
2025-02-13 17:35:54 +0100

    delete all

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 
-def one():
-    print("hello")
 
-def hello():
-    one()
-    print("hello world")
-    two()
 
-def two():
-    print("world")
 
 if __name__ == "__main__":
-    hello()
+    print("test")
